83ccb226f2f0ff78222e01c48d71c9c494c01d4e
Christoph Franke <krito@krito.de>
2018-01-26 01:04:09 +0100

    added jshint to project.

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,22 @@ module.exports = {
       { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'client/src') },
       { test: /pixi\.js/, use: ['expose-loader?PIXI'] },
       { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
-      { test: /p2\.js/, use: ['expose-loader?p2'] }
+      { test: /p2\.js/, use: ['expose-loader?p2'] },
+      {
+        test: /\.js$/, // include .js files
+        enforce: "pre", // preload the jshint loader
+        exclude: /node_modules/, // exclude any and all files in the node_modules folder
+        use: [
+          {
+            loader: "jshint-loader",
+            options: { 
+              camelcase: true, 
+              emitErrors: true, 
+              failOnHint: true
+            } 
+          }
+        ]
+      }
     ]
   },
  	resolve: {
